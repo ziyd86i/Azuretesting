83704b25c491cd25e8a2b4e0cdbd082e81fa2849
ziyd86i <5706021620156@fitm.kmutnb.ac.th>
2018-01-30 19:45:53 +0700

    Set cors

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 // const apiRoutes = express.Router();
 
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use ( (req,res,next) => {
+  // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
 
 app.get('/', (req, res) => {
   res.send('<h1> Hello Node.js</h1>');
